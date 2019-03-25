532d11e642f2c470361f73745c8af8b55092d7a4
wangxiaogang0629 <346775171@qq.com>
2019-03-25 21:06:51 +0800

    dingwei

--- blog/src/js/component/header/header.jsx
+++ blog/src/js/component/header/header.jsx
@@ -18,6 +18,27 @@ class Header extends Component {
     };
   }
 
+  _get = () => {
+    // ecZSQ1IYyurBNhnPjtmM3Bus38NSSOfn
+
+    var map = new BMap.Map("allmap");
+    var point = new BMap.Point(116.331398,39.897445);
+    map.centerAndZoom(point,12);
+
+    var geolocation = new BMap.Geolocation();
+    geolocation.getCurrentPosition(function(r){
+    	if(this.getStatus() == BMAP_STATUS_SUCCESS){
+    		var mk = new BMap.Marker(r.point);
+    		map.addOverlay(mk);
+    		map.panTo(r.point);
+    		alert('您的位置：'+r.point.lng+','+r.point.lat);
+    	}
+    	else {
+    		alert('failed'+this.getStatus());
+    	}
+    });
+  }
+
 
   componentDidMount() {
     let _url = 'https://free-api.heweather.net/s6/weather/now?location=' +
@@ -36,6 +57,8 @@ class Header extends Component {
         console.log(err)
       }
     });
+
+    this._get();
   }
 
   render() {
